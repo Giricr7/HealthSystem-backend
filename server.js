0998ca12a4d0034e26f99d579605afa1ed3a8d35
Giriprasath <giriarjun1995@gmail.com>
2022-03-03 21:13:46 -0700

    first commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 connectDB();
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://health-system.netlify.app",
   credentials:true,     
   optionSuccessStatus:200
 }))
